83b6b1750e48fd8998519e35411c3253775842b4
VetrovDmitry007 <13im@mail.ru>
2023-03-30 08:38:27 +0300

    1. Синонимы основных вопросов. 2. При первичном заполнении, в базу заносятся вопросы, их синонимы и варианты ответов. 3. Настройки перемещены в модуль setup.py

--- SQLite.py
+++ SQLite.py
@@ -21,10 +21,21 @@ class SQLite:
                 self.conn = sqlite3.connect('chatQR.db')
 
     def _createTab(self):
-        print('_createTab: qu_an')
+        # Таблица основных запросов
+        print('_createTab: quest_pr')
         c = self.conn.cursor()
-        c.execute('''CREATE TABLE qu_an(id INTEGER PRIMARY KEY AUTOINCREMENT, date_cr text, question text, 
-        answer_1 text, answer_2 text, answer_3 text, pad_question text)''')
+        c.execute('''CREATE TABLE quest_pr(id INTEGER PRIMARY KEY AUTOINCREMENT, question text, 
+        embed text)''')
+        self.conn.commit()
+        # Таблица вариаций основных запросов
+        print('_createTab: quest_sc')
+        c.execute('''CREATE TABLE quest_sc(id INTEGER PRIMARY KEY AUTOINCREMENT, quest_pr_id INTEGER, question text, 
+        embed text, FOREIGN KEY (quest_pr_id) REFERENCES quest_pr (id))''')
+        self.conn.commit()
+        # Таблица ответов на основные запросы
+        print('_createTab: answer_pr')
+        c.execute('''CREATE TABLE answer_pr(id INTEGER PRIMARY KEY AUTOINCREMENT, quest_pr_id INTEGER, 
+                answer text, FOREIGN KEY (quest_pr_id) REFERENCES quest_pr (id))''')
         self.conn.commit()
 
     def addRec(self, tab, **dc_val):
@@ -34,11 +45,23 @@ class SQLite:
         :return: boool
         """
         c = self.conn.cursor()
-        dc_val.update({'date_cr': datetime.now().strftime("%Y-%m-%d_%H-%M-%S")})
-        col = ', '.join(dc_val.keys())
-        col_val = list(map(lambda s: '"' + s + '"', dc_val.values()))  # добавляем кавычки
-        col_val = ', '.join(col_val)
-        c.execute(f"INSERT INTO {tab}({col}) VALUES ({col_val})")
+        ls_col = list(dc_val.keys())
+        if len(ls_col)>1:
+            col = ', '.join(ls_col)
+        else:
+            col = ls_col[0]
+
+        ls_val = [s.replace('"','') for s in dc_val.values()]
+        ls_val = list(map(lambda s: '"' + s + '"', ls_val))  # добавляем кавычки
+        val = ', '.join(ls_val)
+        # if len(ls_val) > 1:
+        #     ls_val = list(map(lambda s: '"' + s + '"', ls_val))  # добавляем кавычки
+        #     val = ', '.join(ls_val)
+        # else:
+        #     val = ls_val[0]
+
+        sql = f"INSERT INTO {tab}({col}) VALUES ({val})"
+        c.execute(sql)
         self.conn.commit()
         return True
 
@@ -54,8 +77,20 @@ class SQLite:
 
 if __name__ == '__main__':
     sql = SQLite()
-    dc = {'question': 'Где находится организация "Рога и Копыта" '}
-    # sql.simpleReq('delete from qu_an')
-    # sql.addRec('qu_an', **dc)
-    ls = sql.simpleReq('select  * from qu_an')
+    # dc = {'question': 'Где находится организация ~Рога и Копыта~ '}
+    # sql.simpleReq('delete from quest_pr')
+    # sql.addRec('quest_pr', **dc)
+
+    # ls = sql.simpleReq('select  * from quest_pr')
+    # ls = sql.simpleReq('select  * from quest_sc')
+    ls = sql.simpleReq('select  * from answer_pr')
+
+    # fild = 'Какие услуги предоставляет Фирма Рога и копыта?'
+    # ls =sql.simpleReq(f'select id from quest_pr where question = "{fild}"')[0][0]
+    # pprint(ls)
+
+    # dc = {'answer': 'Фирма Рога и Копыта оказывает услуги по организации пеших эротических туров.', 'quest_pr_id': '15'}
+    # sql.addRec('answer_pr', **dc)
+
+    # ls = sql.simpleReq('select  * from answer_pr')
     pprint(ls)
--- chatAPI.py
+++ chatAPI.py
@@ -1,11 +1,16 @@
 """
 Подготовка вопросов и ответов по описанию деятельности фирмы
 """
+from pprint import pprint
+
 import openai
 import re
+
+import torch
 from sentence_transformers import SentenceTransformer, util
 from typing import List, Tuple, Dict
 from SQLite import SQLite
+import setup
 
 model_eval = SentenceTransformer('distiluse-base-multilingual-cased-v1')
 
@@ -13,11 +18,11 @@ model_eval = SentenceTransformer('distiluse-base-multilingual-cased-v1')
 class GPT:
 
     def __init__(self, discr_ltd: str):
-        openai.api_key = "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"
+        openai.api_key = setup.api_key
         self.pre_fix = "В тебя заложена следующая информация."
         self.discr_ltd = discr_ltd
         self.post_fix = "Ты услужливый, творческий, умный и очень дружелюбный ИИ-помощник данной фирмы."
-        self.cn_question = 20
+        self.cn_question = setup.cn_question
         self.sql = SQLite()
 
     def get_quest(self) -> List[str]:
@@ -30,8 +35,22 @@ class GPT:
         res = self.get_response(new_descr)
         ls = res.split('\n')
         # Удаление нумерации
-        ls_quest = list(map(lambda x: re.split(r'\d+[.]{1}', x)[1].strip(), ls))
-        return ls_quest
+        ls = [re.sub(r'\A\d+[.)]{1}', '', s) for s in ls]
+        ls = [s.strip() for s in ls]
+        return ls
+
+    def get_quest_sc(self, quest) -> List[str]:
+        """ Возвращает список синонимов возможных вопросов
+
+        :return:
+        """
+        new_descr = f'{self.discr_ltd} {self.post_fix} Составь 20 вариантов вопроса "{quest}"'
+        res = self.get_response(new_descr)
+        ls = res.split('\n')
+        # Удаление нумерации
+        ls = [re.sub(r'\A\d+[.)]{1}', '', s) for s in ls]
+        ls = [s.strip() for s in ls]
+        return ls
 
     def ins_quest(self, ls_quest: List[str]):
         """ Добавляет возможные вопросы в базу
@@ -40,9 +59,41 @@ class GPT:
         :return:
         """
         for question in ls_quest:
-            question = question.replace('"', "'")
+            # question = question.replace('"', "'")
             dc = {'question': question}
-            self.sql.addRec('qu_an', **dc)
+            self.sql.addRec('quest_pr', **dc)
+
+    def get_quest_id(self, fild:str):
+        """ Получение id вопроса
+
+        :param fild:
+        :return:
+        """
+        # fild = fild.replace('"', "'")
+        id = self.sql.simpleReq(f'select id from quest_pr where question = "{fild}"')[0][0]
+        return str(id)
+
+    def ins_answer(self, question, ls_answer: List[str]):
+        """ Добавляет возможные ответы в базу
+
+        :param ls_quest: список возможных ответов
+        :return:
+        """
+        id = self.get_quest_id(question)
+        for answer in ls_answer:
+            dc = {'answer': answer, 'quest_pr_id':id}
+            self.sql.addRec('answer_pr', **dc)
+
+    def ins_quest_sc(self, question, ls_quest_sc: List[str]):
+        """ Добавляет возможные ответы в базу
+
+        :param ls_quest: список возможных ответов
+        :return:
+        """
+        id = self.get_quest_id(question)
+        for quest in ls_quest_sc:
+            dc = {'question': quest, 'quest_pr_id':id}
+            self.sql.addRec('quest_sc', **dc)
 
     def get_answ(self, quest) -> List[str]:
         """ Возвращает список возможных ответов
@@ -51,18 +102,22 @@ class GPT:
         :return:
         """
         prm = f'{self.pre_fix} {self.discr_ltd} Как по разному могла бы ты ответить на вопрос. "{quest}"' \
-              f' Предоставь не менее 3-х вариантов ответа. В каждом ответе одно предложение.'
+              f' Предоставь не менее 3-х развёрнутых вариантов ответа. В каждом ответе одно предложение.'
         res = self.get_response(prm)
         ls = res.split('\n')
         # Удаление нумерации
-        ls_answer = list(map(lambda x: re.split(r'\d+[.]{1}', x)[1].strip(), ls))
-        return ls_answer
+        ls = [re.sub(r'\A\d+[.)]{1}','',s) for s in ls]
+        ls = [s.strip() for s in ls]
+        return ls
 
     def init_db(self):
         """ Начальное заполнение базы
 
         :return:
         """
+        self.sql.simpleReq('delete from quest_sc')
+        self.sql.simpleReq('delete from answer_pr')
+        self.sql.simpleReq('delete from quest_pr')
         print('Начальное заполнение базы.')
         print('1. Генерация возможных вопросов.')
         ls_quest = self.get_quest()
@@ -70,6 +125,17 @@ class GPT:
         self.ins_quest(ls_quest)
         print('Вопросы занесены в базу.')
         print('2. Генерация возможных ответов.')
+        for cn, quest in enumerate(ls_quest, start=1):
+            print(f'2.{cn}. Генерация ответов на вопрос: {quest}')
+            ls_answ = self.get_answ(quest)
+            self.ins_answer(quest, ls_answ)
+        print('Ответы занесены в базу.')
+        print('3. Генерация вариаций основных вопросов.')
+        for cn, quest in enumerate(ls_quest, start=1):
+            print(f'3.{cn}. Генерация синонимов вопроса: {quest}')
+            ls_quest_sc = self.get_quest_sc(quest)
+            self.ins_quest_sc(quest, ls_quest_sc)
+        print('Синонимы вопросов занесены в базу.')
 
     def get_response(self, msg):
         """ Формирование запроса в ChatGPT
@@ -92,36 +158,104 @@ class GPT:
 
 
 def test_bert():
-    # discr_ltd = """
-    #   Фирма "Рога и копыта" находится в городе Орёл на улице Октябрьская д.10. Директора зовут
-    #   Иванов Сергей Николаевич. Часы работы с 8.00 до 17.00. Обеденный перерыв
-    #   с 13.00 до 14.00. Фирма оказывает услуги по организации пеших эротических туров.
-    #   Телефон приёмной: 8(910)455-99-77.
-    #   """
-    # discr_ltd = "До которого часа вы работаете"
-    discr_ltd = "Как вас найти"
-    query_embedding = model_eval.encode(discr_ltd, device='cpu')
-    passage_embedding = model_eval.encode(["Где находится фирма 'Рога и Копыта'",
-                                           'Каково имя директора фирмы?',
-                                           'Какие услуги предоставляет фирма?',
-                                           'Какие часы работы?',
-                                           'Есть ли обеденный перерыв?'], device='cpu')
+    """ Нахождение ближайшего схожего вопроса
 
-    print("Сходство:", util.dot_score(query_embedding, passage_embedding))
+    """
+    # query = 'Кто ваш директор'
+    query = 'Как вас найти'
+    # query = 'Как до вас доехать'
+    query_embedding = model_eval.encode(query, device='cpu')
+    st ="""
+        Как зовут вашего директора?
+        Где находится ваша фирма?
+        Можете сказать, где расположена ваша компания?
+        Где находится офис вашей компании?
+        Где я могу найти вашу фирму?
+        В каком районе находится ваша компания?
+        Как мне добраться до вашей фирмы?
+        Какой адрес вашей компании?
+        Где я могу найти вашу фирму на карте?
+        Где расположен ваш бизнес-центр?
+        Как я могу найти ваш офис?
+        Где находится ваше представительство?
+        Как мне найти вашу компанию?
+        Где расположена ваша головная офис?
+        Какой индекс у вашего офиса?
+        В каком здании находится ваш офис?
+        Как мне добраться до вашего офиса на общественном транспорте?
+        Где находится ваша основная база?
+        Как я могу найти вашу фирму в Интернете?
+        Где находится ваш центральный офис?
+        Где находится ваш филиал в этом городе?
+        """
+    ls = st.split('\n')
+    passage_embedding = model_eval.encode(ls, device='cpu')
+
+    tensor = util.dot_score(query_embedding, passage_embedding)
+    print("Сходство:", tensor)
+    print("Вопрос:", query)
+    print(f"Схожий вопрос: {ls[torch.argmax(tensor)].strip()}, номер: {torch.argmax(tensor)}")
+    print(f'mean + std/2: {tensor.mean() + tensor.std() / 2}')
+    print(f'std: {tensor.std()}')
+    print(f'mean : {tensor.mean()}')
+    print(f'tensor > 0,5: { tensor[tensor > 0.5]  }')
+
+
+def test_bert_2():
+    """ Косинусная близость между вопросом
+    и средне-арифметическим эмбединга списка аналогичных вопросов
+    """
+    # query_embedding = model_eval.encode('Как вас найти', device='cpu')
+    # query_embedding = model_eval.encode('До которого часа вы работаете', device='cpu')
+    # query_embedding = model_eval.encode('Кто ваш директор', device='cpu')
+    query_embedding = model_eval.encode('Как до вас доехать', device='cpu')
+    st ="""
+        Где находится ваша фирма?
+        Можете сказать, где расположена ваша компания?
+        Где находится офис вашей компании?
+        Где я могу найти вашу фирму?
+        В каком районе находится ваша компания?
+        Как мне добраться до вашей фирмы?
+        Какой адрес вашей компании?
+        Где я могу найти вашу фирму на карте?
+        Где расположен ваш бизнес-центр?
+        Как я могу найти ваш офис?
+        Где находится ваше представительство?
+        Как мне найти вашу компанию?
+        Где расположена ваша головная офис?
+        Какой индекс у вашего офиса?
+        В каком здании находится ваш офис?
+        Как мне добраться до вашего офиса на общественном транспорте?
+        Где находится ваша основная база?
+        Как я могу найти вашу фирму в Интернете?
+        Где находится ваш центральный офис?
+        Где находится ваш филиал в этом городе?
+        """
+    ls = st.split('\n')
+    ls_query = [s.strip() for s in ls if len(s)>1]
+    print(ls_query)
+
+    passage_embedding = model_eval.encode(ls_query, device='cpu')
+    mean_embedding = passage_embedding.mean(axis=0)
+
+    print("Сходство:", util.dot_score(query_embedding, mean_embedding))
 
 
 if __name__ == '__main__':
     discr_ltd = """
-      Фирма "Рога и копыта" находится в городе Орёл на улице Октябрьская д.10. Директора зовут 
-      Иванов Сергей Николаевич. Часы работы с 8.00 до 17.00. Обеденный перерыв 
-      с 13.00 до 14.00. Фирма оказывает услуги по организации пеших эротических туров. 
-      Телефон приёмной: 8(910)455-99-77.   
+      Фирма Рога и копыта находится в городе Орёл на улице Октябрьская д.10. Директора зовут
+      Иванов Сергей Николаевич. Часы работы с 8.00 до 17.00. Обеденный перерыв
+      с 13.00 до 14.00. Фирма оказывает услуги по организации пеших эротических туров.
+      Телефон приёмной: 8(910)455-99-77.
       """
-    # gpt = GPT(discr_ltd)
-    # gpt.init_db()
+    gpt = GPT(discr_ltd)
+    gpt.init_db()
     # ls = gpt.get_quest()
-    # print(ls[10])
-    # ls = gpt.get_answ('Предлагаются ли групповые туры?')
-    # ls = gpt.sql.simpleReq('select  question from qu_an')
-    # print(ls)
-    test_bert()
+    # pprint(ls)
+    # ls = gpt.get_quest_sc(ls[0])
+    # ls = gpt.get_quest_sc("Какие услуги оказывает Фирма Рога и копыта?")
+    # ls = gpt.sql.simpleReq('select question from qu_an')
+    # pprint(ls)
+
+    # test_bert()
+    # test_bert_2()
